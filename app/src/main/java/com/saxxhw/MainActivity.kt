4e7858b1abe60b25fd9603cf136707b5d8456f84
Saxxhw <wang921018>
2018-01-03 10:44:05 +0800

    1.1.0 1、优化底部导航菜单配置方式 2、简化点击事件注册方法

--- app/src/main/java/com/saxxhw/MainActivity.kt
+++ app/src/main/java/com/saxxhw/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.saxxhw
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.widget.Toolbar
+import android.view.View
 import com.saxxhw.library.base.BaseActivity
-import com.saxxhw.library.state.StateLayout
+import com.saxxhw.library.widget.BottomNavigationBar
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), BottomNavigationBar.MenuItemClickListener {
 
     override fun getLayout(): Int = R.layout.activity_main
 
-    override fun getStateLayout(): StateLayout? = stateLayout
-
     override fun initEventAndData(savedInstanceState: Bundle?) {
-        showErrorState()
+        Handler().postDelayed({ bottomMenu.setXmlResource(R.xml.bottombar_tabs_color_changing2) }, 1000 * 12)
+    }
+
+    override fun bindListener() {
+        bottomMenu.setOnMenuItemClickListener(this)
     }
 
     override fun setNavigation(mToolBar: Toolbar?) {
@@ -22,7 +26,10 @@ class MainActivity : BaseActivity() {
         mToolBar?.setNavigationOnClickListener { toast("个人中心") }
     }
 
-    override fun onRetrieve() {
-        toast("重新获取数据")
+    override fun onItemClick(view: View) {
+        when (view.id) {
+            R.id.tab_recharge -> toast("充值")
+            R.id.tab_repair -> toast("报修")
+        }
     }
 }
--- library/src/main/java/com/saxxhw/library/widget/BottomNavigationBar.kt
+++ library/src/main/java/com/saxxhw/library/widget/BottomNavigationBar.kt
@@ -1,13 +1,16 @@
 package com.saxxhw.library.widget
 
 import android.content.Context
+import android.content.res.Resources
 import android.content.res.XmlResourceParser
 import android.support.annotation.IntRange
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.CheckBox
+import android.widget.ImageView
 import android.widget.LinearLayout
 import com.saxxhw.library.R
 import kotlinx.android.synthetic.main.widget_bottom_navigation_bar.view.*
@@ -22,6 +25,8 @@ import java.io.IOException
 class BottomNavigationBar : LinearLayout {
 
     companion object {
+        private val DEFAULT_XML_RESOURCE = 0
+
         private val TAB_TAG = "tab"
         private val RESOURCE_NOT_FOUND = 0
 
@@ -33,20 +38,21 @@ class BottomNavigationBar : LinearLayout {
     // 上下文对象
     private var mContext: Context
 
-    // CheckBox集合
-    private lateinit var cbArray: Array<CheckBox>
+    // 全部控件集合
+    private lateinit var viewArray: Array<View>
 
     // 配置文件资源id
-    private val tabXmlResource: Int
+    private var tabXmlResource: Int
+
     // xml解析器
-    private var parser: XmlResourceParser
+    private lateinit var parser: XmlResourceParser
 
     // 点击事件
     private var menuItemClickListener: MenuItemClickListener? = null
 
     constructor(context: Context) : this(context, null)
 
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, DEFAULT_XML_RESOURCE)
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         mContext = context
@@ -57,14 +63,9 @@ class BottomNavigationBar : LinearLayout {
         // 初始化
         val ta = context.theme.obtainStyledAttributes(attrs, R.styleable.BottomNavigationBar, defStyleAttr, 0)
 
-        // 中间图片
-        val centerImage = ta.getResourceId(R.styleable.BottomNavigationBar_bb_centerSrc, 0)
-        ivCenter.setImageResource(centerImage)
-
         // 其他按钮
         tabXmlResource = ta.getResourceId(R.styleable.BottomNavigationBar_bb_tabXmlResource, 0)
-        this.parser = context.resources.getXml(tabXmlResource)
-        parseTabs()
+        setXmlResource(tabXmlResource)
 
         // 绑定监听器
         bindListener()
@@ -74,16 +75,17 @@ class BottomNavigationBar : LinearLayout {
      * 绑定监听事件
      */
     private fun bindListener() {
+        val cbArray = arrayOf(cb1, cb2, cb3, cb4)
         arrayOf(rlyt1, rlyt2, rlyt3, rlyt4)
                 .forEachIndexed { index, relativeLayout ->
                     relativeLayout.setOnClickListener {
                         val cb = cbArray[index]
                         if (cb.setChecked()) {
-                            menuItemClickListener?.onItemClick(cb.id)
+                            menuItemClickListener?.onItemClick(cb)
                         }
                     }
                 }
-        ivCenter.setOnClickListener { menuItemClickListener?.onCenterClick() }
+        ivCenter.setOnClickListener { menuItemClickListener?.onItemClick(ivCenter) }
         rlyt1.performClick()
     }
 
@@ -91,7 +93,7 @@ class BottomNavigationBar : LinearLayout {
      * 解析xml文件
      */
     private fun parseTabs() {
-        cbArray = arrayOf(cb1, cb2, cb3, cb4)
+        viewArray = arrayOf(cb1, cb2, ivCenter, cb3, cb4)
         try {
             var index = 0
             var eventType: Int
@@ -99,7 +101,7 @@ class BottomNavigationBar : LinearLayout {
                 eventType = parser.next()
                 if (eventType == XmlResourceParser.START_TAG && TAB_TAG == parser.name) {
                     println(eventType)
-                    parseNewTab(parser, cbArray[index++])
+                    parseNewTab(parser, viewArray[index++])
                 }
             } while (eventType != XmlResourceParser.END_DOCUMENT)
         } catch (e: IOException) {
@@ -114,23 +116,29 @@ class BottomNavigationBar : LinearLayout {
     /**
      * 将解析出的数据赋值给控件
      */
-    private fun parseNewTab(parser: XmlResourceParser, checkBox: CheckBox) {
+    private fun parseNewTab(parser: XmlResourceParser, view: View) {
         val numberOfAttributes = parser.attributeCount
         for (i in 0 until numberOfAttributes) {
             val attrName = parser.getAttributeName(i)
             when (attrName) {
                 ID -> {
                     val id = parser.getIdAttributeResourceValue(i)
-                    checkBox.id = id
+                    view.id = id
                 }
                 ICON -> {
                     val icon = parser.getAttributeResourceValue(i, RESOURCE_NOT_FOUND)
-                    val drawable = ContextCompat.getDrawable(mContext, icon)
-                    checkBox.setCompoundDrawablesWithIntrinsicBounds(null, drawable, null, null)
+                    if (view is CheckBox) {
+                        val drawable = ContextCompat.getDrawable(mContext, icon)
+                        view.setCompoundDrawablesWithIntrinsicBounds(null, drawable, null, null)
+                    } else if (view is ImageView) {
+                        view.setImageResource(icon)
+                    }
                 }
                 TITLE -> {
-                    val title = getTitleValue(parser, i)
-                    checkBox.text = title
+                    if (view is CheckBox) {
+                        val title = getTitleValue(parser, i)
+                        view.text = title
+                    }
                 }
             }
         }
@@ -168,8 +176,7 @@ class BottomNavigationBar : LinearLayout {
      * 监控事件
      */
     interface MenuItemClickListener {
-        fun onItemClick(id: Int)
-        fun onCenterClick()
+        fun onItemClick(view: View)
     }
 
     class TabParserException : RuntimeException()
@@ -184,9 +191,13 @@ class BottomNavigationBar : LinearLayout {
     }
 
     /**
-     * 设置中间图片资源
+     * 设置xml资源id
      */
-    fun setCenterImage(img: Int) {
-        ivCenter.setImageResource(img)
+    fun setXmlResource(resId: Int) {
+        if (DEFAULT_XML_RESOURCE != resId) {
+            this.tabXmlResource = resId
+            this.parser = context.resources.getXml(tabXmlResource)
+            parseTabs()
+        }
     }
 }
